fac6d84bc1b7865759f69f9c3605038e7a579dbe
senft <senftt@gmail.com>
2014-01-04 17:25:42 +0100

    Different icons for lower volumes

--- .bin/volume-dzen.sh
+++ .bin/volume-dzen.sh
@@ -3,5 +3,12 @@ COLOR_ON="#eddcd3"
 COLOR_OFF="#989584"
 BAR_MAX_WIDTH=40
 BAR_HEIGHT=7
-vol=`amixer get Master | grep "\[on\]" | egrep -o "[0-9]+%" | head -1 | sed 's/%//'`
+vol=`amixer get Master | egrep -o "[0-9]+%" | head -1 | sed 's/%//'`
+mute=`amixer get Master | grep "\[on\]"`
+
+if [ -n "$mute" ]; then
+    printf "^i(/home/jln/.icons/xbm8x8/spkr_01.xbm) "
+else
+    printf "^i(/home/jln/.icons/xbm8x8/spkr_01_mute.xbm) "
+fi
 echo $vol | gdbar -h $BAR_HEIGHT -w $BAR_MAX_WIDTH -fg $COLOR_ON -bg $COLOR_OFF
