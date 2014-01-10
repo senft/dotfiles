be550d94df2388f7f3de8990e4cddef51e4b0427
senft <senftt@gmail.com>
2014-01-10 11:12:19 +0100

    Fixed dzen popup scripts

--- .bin/dzen-spawner.sh
+++ .bin/dzen-spawner.sh
@@ -2,6 +2,12 @@
 
 . ~/.dzen-popup-config.sh
 
+for pid in `pgrep -f -x "/bin/sh /home/jln/.bin/dzen-kill-spawner.sh"`
+do
+    #echo "kill running dzen-kill-spawner.sh with pid $pid"
+    kill "$pid"
+done
+
 SCREEN_WIDTH=$(sres -W)
 
 CAL_START=$((SCREEN_WIDTH-110))
@@ -12,6 +18,14 @@ MUSIC_END=$CAL_START
 
 while true; do
     XPOS=$(xdotool getmouselocation 2> /dev/null | tail -1 | awk -F " " '{print $1}' | cut -d ":" -f 2)
+    YPOS=$(xdotool getmouselocation 2> /dev/null | tail -1 | awk -F " " '{print $2}' | cut -d ":" -f 2)
+
+    #echo $YPOS
+    #echo $BAR_HEIGHT
+    if [ $YPOS -gt $BAR_HEIGHT ]; then
+        #echo "meh"
+        continue
+    fi
 
     if [ $XPOS -gt $CAL_START -a $XPOS -lt $CAL_END ]; then
         pid=$(pgrep -f "dzen-popup-cal")
