b11180adb727974316bf6b5a844da5cc228c9be0
senft <senftt@gmail.com>
2014-01-29 10:11:28 +0100

    Updated volume scripts

--- .bin/volume.sh
+++ .bin/volume.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
-amixer get Master | grep "\[on\]" | egrep -o "[0-9]+%" | head -1
+out=$(amixer get Master | grep "\[on\]" | egrep -o "[0-9]+%" | head -1 | cut -d % -f 1)
+if [ -z "$out" ]; then
+    echo "mute"
+else
+    echo "$out"
+fi
